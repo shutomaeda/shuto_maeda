4f15f7301d41774cee12b6462845dd382be72927
shuto012 <m.shuto012@gmail.com>
2024-05-21 19:01:30 +0900

    全体平均点の追加・科目別平均点修正

--- src/curriculum_B/Qes7.java
+++ src/curriculum_B/Qes7.java
@@ -7,10 +7,10 @@ public class Qes7 {
 		System.out.print("生徒の人数を入力してください(2人以上):");
 		Scanner scan = new Scanner(System.in);
 		int student = scan.nextInt();
-		int[] english = new int[student];
-		int[] math = new int[student]; 
-		int[] science = new int[student];
-		int[] society = new int[student];
+		double[] english = new double[student];
+		double[] math = new double[student]; 
+		double[] science = new double[student];
+		double[] society = new double[student];
 		//　点数入力
 		for(int i = 0;i < student;i++) {
 			System.out.print((i+1)+"人目の『英語』の点数を入力してください：");
@@ -32,31 +32,29 @@ public class Qes7 {
 		}
 		System.out.println();
 		//　科目ごとの平均点
-		double english_avg = 0; 
-		double math_avg = 0;
-		double science_avg = 0;
-		double society_avg = 0;
+		double english_all = 0; 
+		double math_all = 0;
+		double science_all = 0;
+		double society_all = 0;
 		for(int i=0;i < student;i++) {
-			 english_avg += english[i];
+			 english_all += english[i];
+			 math_all += math[i];
+			 science_all += science[i];
+			 society_all += society[i];
 		}
-		for(int i=0;i < student;i++) {
-			 math_avg += math[i];
-		}
-		for(int i=0;i < student;i++) {
-			 science_avg += science[i];
-		}
-		for(int i=0;i < student;i++) {
-			 society_avg += society[i];
-		}
-		String eng_avg = String.format("%.2f", english_avg/4);
-		String mat_avg = String.format("%.2f", math_avg/4);
-		String sci_avg = String.format("%.2f", science_avg/4);
-		String soc_avg = String.format("%.2f", society_avg/4);
+
+		String eng_avg = String.format("%.2f", english_all/student);
+		String mat_avg = String.format("%.2f", math_all/student);
+		String sci_avg = String.format("%.2f", science_all/student);
+		String soc_avg = String.format("%.2f", society_all/student);
 		System.out.println("英語の平均点は"+eng_avg+"点です。");
-		System.out.println("英語の平均点は"+mat_avg+"点です。");
-		System.out.println("英語の平均点は"+sci_avg+"点です。");
-		System.out.println("英語の平均点は"+soc_avg+"点です。");
-		
+		System.out.println("数学の平均点は"+mat_avg+"点です。");
+		System.out.println("理科の平均点は"+sci_avg+"点です。");
+		System.out.println("社会の平均点は"+soc_avg+"点です。");
+		//全体の平均点
+		double subject_all = english_all + math_all + science_all + society_all;
+		String all_avg = String.format("%.2f",subject_all/(student*4));
+		System.out.print("全体の平均点は"+all_avg+"点です。");
 	}
 
 }
