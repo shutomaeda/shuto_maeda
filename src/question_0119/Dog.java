406cc6dc68c86e8dd11107c865e65d3f1a023eb0
shuto012 <m.shuto012@gmail.com>
2024-05-28 10:19:23 +0900

    課題1-18修正、1-19Dogコミット

---
+++ src/question_0119/Dog.java
@@ -0,0 +1,19 @@
+package question_0119;
+
+public class Dog {
+
+	// Q1：フィールドに動物の名前の変数を定義してください。
+	String name = null;
+	// Q2：フィールドに動物の数の変数を定義してください。
+	int num = 0; 
+	// Q3：Q1で作成した変数に「犬」を代入するコンストラクタを作成してください。
+	public Dog() {
+		this.name = "犬";
+	}
+	public Dog(int num) {
+		this.num = num;
+	}
+	// Q4：Q2で作成した変数に引数を代入するコンストラクタを作成してください。
+		// TODO 自動生成されたコンストラクター・スタ
+
+}
